e05246fc2c04f495442d58fb3e1b8890b769a52d
Rehan2005 <rehan2005hussain@gmail.com>
2025-03-22 22:34:53 +0530

    Animation added on login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -49,6 +49,10 @@ const Login = () => {
             <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
               <span>TaskMate</span>
             </p>
+
+            <div className='cell'>
+              <div className='circle rotate-in-up-left'></div>
+            </div>
           </div>
         </div>
 
